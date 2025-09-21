d21c8a8a2479e0117d221f0179c43a0d2d611f3e
reysilvaa <reynaldsilva123@gmail.com>
2025-09-21 20:49:54 +0700

    Perbarui versi paket menjadi 1.0.1, ubah tipe menjadi module, dan perbarui URL repositori serta homepage. Modifikasi konfigurasi Rollup untuk menggunakan sintaks import. Perbaiki penggunaan optional chaining pada validasi matriks di beberapa file.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-const typescript = require("@rollup/plugin-typescript");
-const resolve = require("@rollup/plugin-node-resolve");
-const commonjs = require("@rollup/plugin-commonjs");
-const dts = require("rollup-plugin-dts");
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
-module.exports = [
+export default [
   // CommonJS build
   {
     input: "src/index.ts",
@@ -71,6 +71,6 @@ module.exports = [
       file: "lib/index.d.ts",
       format: "es",
     },
-    plugins: [dts.default()],
+    plugins: [dts()],
   },
 ];
